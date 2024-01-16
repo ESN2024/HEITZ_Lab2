fa16f016fb4dbb8e6c8eff23fbf9f55b42378113
Kilian HEITZ <kiheitz>
2024-01-16 17:31:42 +0100

    fin

--- Lab_2_top.vhd
+++ Lab_2_top.vhd
@@ -1,19 +1,51 @@
 
 
 
+	architecture Lab_2_top is
+	
+				port ( clock,reset : in std_logic;
+						 segdecod1_o,segdecod2_o,segdecod3_o : out std_logic_vector (6 downto 0)
+					  );
+	end Lab_2_top;
+
+
+	component segdecod is
+	
+				PORT (
+						entree: in std_logic_vector(3 DOWNTO 0);
+						sortie: out std_logic_vector(6 DOWNTO 0)
+						);	
+	end component 
 
 
     component Lab_2 is
-        port (
+        port 
+		  (
             clk_clk                          : in  std_logic                     := 'X'; -- clk
             pio_0_external_connection_export : out std_logic_vector(11 downto 0);        -- export
             reset_reset_n                    : in  std_logic                     := 'X'  -- reset_n
         );
     end component Lab_2;
 
+	 
+	 	 
+	 begin
+	 
     u0 : component Lab_2
-        port map (
+        port map 
+		  (
             clk_clk                          => CONNECTED_TO_clk_clk,                          --                       clk.clk
             pio_0_external_connection_export => CONNECTED_TO_pio_0_external_connection_export, -- pio_0_external_connection.export
             reset_reset_n                    => CONNECTED_TO_reset_reset_n                     --                     reset.reset_n
         );
+		  
+	 u1 : component segdecod is
+	 PORT (
+						entree: in std_logic_vector(3 DOWNTO 0);
+						sortie: out std_logic_vector(6 DOWNTO 0)
+						);	
+			
+		  
+		  
+		
+	 end structurel
