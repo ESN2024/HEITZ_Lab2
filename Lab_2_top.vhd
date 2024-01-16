6152734f3eb7b309b10d3359b5bc69e9a754c53a
kilian-HTZ <kilian.heitz@etu.uca.fr>
2024-01-17 00:08:10 +0100

    tout est prêt juste le code en C à faire

--- Lab_2_top.vhd
+++ Lab_2_top.vhd
@@ -1,24 +1,28 @@
+library ieee;
+Use ieee.std_logic_1164.all;
+Use ieee.numeric_std.all;
 
-
-
-	architecture Lab_2_top is
-	
+	entity Lab_2_top is
+				
+				
 				port ( clock,reset : in std_logic;
 						 segdecod1_o,segdecod2_o,segdecod3_o : out std_logic_vector (6 downto 0)
 					  );
 	end Lab_2_top;
 
+	architecture structurel of Lab_2_top is
+	
 
-	component segdecod is
+	component segdecod
 	
 				PORT (
 						entree: in std_logic_vector(3 DOWNTO 0);
 						sortie: out std_logic_vector(6 DOWNTO 0)
 						);	
-	end component 
+	end component;
 
 
-    component Lab_2 is
+    component Lab_2
         port 
 		  (
             clk_clk                          : in  std_logic                     := 'X'; -- clk
@@ -27,25 +31,36 @@
         );
     end component Lab_2;
 
-	 
+	 --internal signals declaration
+	 signal f_s : std_logic_vector(11 downto 0);
 	 	 
 	 begin
 	 
-    u0 : component Lab_2
-        port map 
-		  (
-            clk_clk                          => CONNECTED_TO_clk_clk,                          --                       clk.clk
-            pio_0_external_connection_export => CONNECTED_TO_pio_0_external_connection_export, -- pio_0_external_connection.export
-            reset_reset_n                    => CONNECTED_TO_reset_reset_n                     --                     reset.reset_n
-        );
+    U0 : Lab_2
+			port map 
+						(
+						clk_clk                          => clock,  -- clk.clk
+						pio_0_external_connection_export => f_s, 	  -- pio_0_external_connection.export
+						reset_reset_n                    => reset   -- reset.reset_n
+						);
 		  
-	 u1 : component segdecod is
-	 PORT (
-						entree: in std_logic_vector(3 DOWNTO 0);
-						sortie: out std_logic_vector(6 DOWNTO 0)
+	 U1 : segdecod
+			port map (
+						entree => f_s(3 downto 0),
+						sortie => segdecod1_o
+						);
+		
+	 U2 : segdecod
+			port map (
+						entree => f_s(7 downto 4),
+						sortie => segdecod2_o
 						);	
 			
-		  
-		  
 		
-	 end structurel
+	 U3 : segdecod
+			port map (
+						entree => f_s(11 downto 8),
+						sortie => segdecod3_o
+						);	
+					
+	 end structurel;
---
+++ software/Lab_2/main.c
@@ -0,0 +1,6 @@
+#include <system.h>
+
+int main (void)
+{
+	return 0;
+}
