6a439f70ed18f909b59e9d1d663cb1d16ee9ef50
Kilian HEITZ <kiheitz>
2024-01-16 17:29:52 +0100

    vhdl fait qsys fait manque le C

---
+++ Lab_2/Lab_2_bb.v
@@ -0,0 +1,10 @@
+
+module Lab_2 (
+	clk_clk,
+	pio_0_external_connection_export,
+	reset_reset_n);	
+
+	input		clk_clk;
+	output	[11:0]	pio_0_external_connection_export;
+	input		reset_reset_n;
+endmodule
